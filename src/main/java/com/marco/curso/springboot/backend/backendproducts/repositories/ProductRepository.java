e7bf852d8fee42724e1f05ddd709c3bbc9cb9e1c
marcoantoniolomu <marcoantoniolomu@gmail.com>
2024-04-01 17:18:15 +0200

    updated

---
+++ src/main/java/com/marco/curso/springboot/backend/backendproducts/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.marco.curso.springboot.backend.backendproducts.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.marco.curso.springboot.backend.backendproducts.entities.Product;
+
+@CrossOrigin({"http://localhost:5173/", "http://localhost:4200/"})
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
